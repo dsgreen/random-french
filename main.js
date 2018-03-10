e54ff2331c285a3c5c971e517327249a8747291b
Doug Green <doug@dsgreen.com>
2018-03-10 15:20:09 -0500

    Add some French, output to HTML

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 var phrases = [
-  'hello',
-  'hi',
-  'oh my',
-  'goodbye'
+  'Bonjour!',
+  'Salut!',
+  'Mon Dieu.',
+  'Au revoir.'
 ];
 
 var rand;
 
-document.getElementById('btn').addEventListener("mouseup", generate);
+// empty french div, add some space
+document.getElementById('french').innerHTML = '&nbsp;';
 
+// add event listeners
+document.getElementById('btn').addEventListener("mouseup", generate);
+document.getElementById('btn').addEventListener("keyup", generate);
+
+// create a random number based on length of array, and output phrase to HTML
 function generate() {
   rand = Math.floor(Math.random() * Math.floor(phrases.length));
-  console.log(rand);
+  // console.log(rand);
+  document.getElementById('french').innerHTML = phrases[rand];
 }
